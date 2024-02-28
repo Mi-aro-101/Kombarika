3ff1173c8a28878605bc2f9976c92c9cbdfbbd6a
Mi-aro-101 <miaroramananadraitsiory@gmail.com>
2024-02-28 23:46:37 +0530

    First commit

---
+++ src/main/java/ambovombe/kombarika/Test.java
@@ -0,0 +1,61 @@
+package ambovombe.kombarika;
+/*entity
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import ambovombe.kombarika.generator.CodeGenerator;
+import ambovombe.kombarika.generator.service.DbService;
+
+import java.util.HashMap;
+/**
+ *
+ *  @author Mamisoa
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     * @throws SQLException
+     */
+     
+    public static void main(String[] args) throws Exception {
+        CodeGenerator codeGenerator = new CodeGenerator();  
+        String path = "Generated";
+        String viewPath = "/home/mamisoa/ITU/L3/Mr_Naina/REACT/crud/src/components";
+        String framework = "csharp:dotnet";
+        String packageName = "ecole";
+        String entity = "entity";
+        String controller = "controller";
+        String repository = "repository";
+        String view = "";
+        String viewType = "react";
+        String url = "http://localhost:8080/";
+        try{
+            // String[] tables = {"district","region"};
+            // DbConnection dbConnection = codeGenerator.getDbConnection();
+            // String str = dbConnection.getListConnection().get(dbConnection.getInUseConnection()).getDatabaseType().getForeignKeyQuery();
+            // str = str.replace("?", "commune");
+            // System.out.println(str);
+            // HashMap<String, String> foreignKeys = DbService.getForeignKeys(dbConnection, "commune");
+            // for (Map.Entry<String, String> set : foreignKeys.entrySet()) {
+            //     System.out.println(set.getKey() + " " + set.getValue());
+            // }
+            String[] tables = DbService.getAllTablesArrays(codeGenerator.getDbConnection());
+            // for(String table: tables)
+            //     System.out.println(table);
+            
+            /* My test */
+            codeGenerator.generateAllEntity(path, tables, packageName, entity, framework);
+            codeGenerator.generateAllRepository(path, tables, packageName, entity, repository, framework);
+            codeGenerator.generateAllController(path, tables, packageName, entity, controller, repository, framework);
+            
+            /* Main test */
+//            codeGenerator.generateAll(path, viewPath, packageName, entity, controller, repository, view, viewType, url, tables, framework);
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            codeGenerator.getDbConnection().close();
+        }    
+    }
+}
---
+++ src/main/java/ambovombe/kombarika/configuration/mapping/ControllerProperty.java
@@ -0,0 +1,24 @@
+package ambovombe.kombarika.configuration.mapping;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ControllerProperty {
+    String path;
+    String get;
+    String post;
+    String put;
+    String delete;
+    String name;
+    String classSyntax;
+    String pathSyntax;
+    String field;
+    String returnType;
+    String returnTypeDelete;
+    String annotationField;
+    String annotationMethodParameter;
+    String annotationArgumentParameterFormData;
+    String annotationArgumentParameterLink;
+    String constructor;
+}
